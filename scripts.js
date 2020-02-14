dde3b48e7370b22814693d6f20f28777e6275773
Daniel <twitchingdan@gmail.com>
2020-02-14 11:27:49 +0100

    New JS

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 function playjjba() { 
-  var jjba = document.getElementById("himniv2"); 
-  jjba.play(); 
+	var jjba = document.getElementById("himniv2"); 
+	jjba.play(); 
 } 
 function pausejjba() {
-var jjba = document.getElementById("himniv2");  
-  jjba.pause(); 
+	var jjba = document.getElementById("himniv2");  
+	jjba.pause(); 
 } 
+function resetjjba() {
+	var jjba = document.getElementById("himniv2");
+	jjba.reset();
+}
 function playriff() { 
   var riff = document.getElementById("riff"); 
   riff.play(); 
+}
+function pranimi() {
+	var pranimi = document.getElementById("pranimi");
+	if (pranimi.checked == false){
+	alert ("Ça bo mo burr?")
 }
