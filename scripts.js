08409d0bb7a7afa530ebabe732340601629fae1b
Daniel <twitchingdan@gmail.com>
2020-02-29 13:03:14 +0100

    Hey CEO of JavaScript,
    I made you a sandwich

--- scripts.js
+++ scripts.js
@@ -17,15 +17,16 @@ function playriff() {
 function leku() {
 	alert ("Lek Plepi dhe Linda Morina nxorrën këngën e re!")
 }
+function lekcheck(){
+	var timecheck;
+	timecheck = setInterval(lekalert, 500);
+}
 function lekalert() {
 	var date = new Date;
 	var s = date.getSeconds();
 	var m = date.getMinutes();
 	var h = date.getHours();
-	if (h === 18 && m === 29 && s === 50){
+	if(h == 18 && m == 29 && s == 50) {
 		alert("Është ora 18 e 30 pa 10 sekonda!");
-	}
-	else{
-		alert('lll');
-	}
+	} else {}
 }
